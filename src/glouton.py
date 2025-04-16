d1fdfe94679f2b10db485a99c4f8ab0f663101f1
Thomas Genoud <thomy672005@gmail.com>
2025-04-16 14:05:04 +0000

    Remove unused docstring from GloutonKnapsackSolver class

--- src/glouton.py
+++ src/glouton.py
@@ -6,20 +6,7 @@ from more_itertools import dotproduct
 from knapsack import KnapsackInstance, KnapsackSolver
 
 class GloutonKnapsackSolver(KnapsackSolver):
-    """
-    >>> kp = KnapsackInstance(W=[13, 13, 13, 10, 24, 11], V=[2600, 2600, 2600, 500, 4500, 960], C=50)
-    >>> bfs = BruteforceKnapsackSolver(kp)
-    >>> Xopt = bfs.solve()
-    >>> Xopt in [(0, 1, 1, 0, 1, 0), (1, 0, 1, 0, 1, 0), (1, 1, 0, 0, 1, 0)]
-    True
-    >>> bfs.value(Xopt)
-    9700
-    >>> bfs.weight(Xopt)
-    50
-    >>> bfs.weight(Xopt) <= bfs._inst.C
-    True
 
-    """
     
     def __init__(self, instance:KnapsackInstance) -> None:
         # TODO: write the constructor by calling the parent class constructor
